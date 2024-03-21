fa25cfadd296566da5ec412322a0915187c6f348
CORP\srosano <scott.rosano@capgemini.com>
2024-03-21 15:28:19 +1100

    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES('Account-1','Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES('Account-2','Banana Republic','');
+CREATE TABLE "Delivery_Item__c" (
+	id VARCHAR(255) NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES('Delivery_Item__c-1','2024-04-06','Non-refrigerated','Bananas','Delivery__c-1');
+CREATE TABLE "Delivery__c" (
+	id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES('Delivery__c-1','Bananas','2024-03-29T01:00:00.000+0000','Requested','Account-2');
+COMMIT;
